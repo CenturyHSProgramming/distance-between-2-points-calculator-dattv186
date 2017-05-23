b319b4c7f8ba9710303cca08a0b85c03cc746dda
Datta_Varsha <dattv186@hsd.k12.or.us>
2017-05-23 08:31:41 -0700

    adding successful code

--- DistanceBetweenTwoPointsCalculator.py
+++ DistanceBetweenTwoPointsCalculator.py
@@ -8,7 +8,32 @@
 # Define Function below
 # be sure to return an integer
 
+import math
+
+
+def calculateDistanceBetween(x1, y1, x2, y2):
+    calculateDistanceBetween = ((x2 - x1)**2) + ((y2 - y1)**2)
+    from math import sqrt as square_root
+    calculateDistanceBetween = square_root(calculateDistanceBetween)
+    calculateDistanceBetween = round(float(calculateDistanceBetween), 2)
+    return(calculateDistanceBetween)
+
+
+
+
+
+
+
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
-    pass # remove or comment out this line if you wish to test the function
+
+    x1 = int(input('Enter a number for x1:    '))
+    x2 = int(input('Enter a number for x2:    '))
+    y1 = int(input('Enter a number for y1:    '))
+    y2 = int(input('Enter a number for y2:    '))
+    distance = calculateDistanceBetween(x1, y1, x2, y2)
+    print(distance)
+    
+    
+             
